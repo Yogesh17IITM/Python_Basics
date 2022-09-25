1934c8e43631e1f39c3dd658a79acbbb64ac042d
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-09-25 10:12:35 +0530

    Added type conversion

--- Program_1_BasicProgram.py
+++ Program_1_BasicProgram.py
@@ -9,13 +9,27 @@ Concepts:
 """
 
 #Printing
-from time import process_time_ns
-
-
 print("Hello")
 
 #Datatypes (any type it can take)
-a = 5
-f = 2.25
-s = "Hai"
-print(a,f,s)
+a = 5               # int
+f = 2.25            # float
+s = "Hai"           # string
+c = 3+4j            # complex
+b = True            # bool
+binary = 0B0110     # binary
+hexadecimal = 0XFF  # Hexadecimal
+
+# Printing all values together
+print(a,f,s,c,b, binary, hexadecimal, (9>7))
+
+# to print which data type the member is
+print(type(a))
+print(type(f))
+print(type(c))
+print(type(b))
+print(type(binary))
+
+# type conversion
+var_a = int(f)  # converting float into int
+print('Var: ' , var_a)

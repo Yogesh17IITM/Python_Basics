17236e26c3f812838e143e5fe0215a05fb037853
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-09-25 09:37:55 +0530

    create new file

---
+++ Program_1_BasicProgram.py
@@ -0,0 +1,21 @@
+# Simple Python program
+
+""" 
+Program to demonstrate a simple python program
+
+Concepts:
+    How to print?
+    Datatypes
+"""
+
+#Printing
+from time import process_time_ns
+
+
+print("Hello")
+
+#Datatypes (any type it can take)
+a = 5
+f = 2.25
+s = "Hai"
+print(a,f,s)
